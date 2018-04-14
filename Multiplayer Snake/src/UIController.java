42c7708dcd36bca74c627aea0b78a34a2d1eb8ca
Shyam Pindoria <pindoria.shyam@icloud.com>
2018-04-14 23:30:33 +0930

    Added code for start button

--- Multiplayer Snake/src/UIController.java
+++ Multiplayer Snake/src/UIController.java
@@ -25,10 +25,10 @@ public class UIController extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 
 
-	JPanel contentPane;
-	JPanel gamePane;
-	JPanel startPane;
-	JPanel loginPane;
+	private JPanel contentPane;
+	private JPanel gamePane;
+	private JPanel startPane;
+	private JPanel loginPane;
 
 	private JPanel boardPane;
 	private JPanel scorePane;
@@ -44,26 +44,24 @@ public class UIController extends JFrame implements ActionListener {
 
 	public UIController() {
 		
-		setTitle("Multiplayer Snake");
+		this.setTitle("Multiplayer Snake");
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1000, 700);
-		setResizable(false);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(100, 100, 1000, 700);
+		this.setResizable(false);
+		this.contentPane = new JPanel();
+		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.setContentPane(this.contentPane);
 		
-		contentCardLayout = new CardLayout(0, 0);
+		this.contentCardLayout = new CardLayout(0, 0);
 		
-		contentPane.setLayout(contentCardLayout);
+		this.contentPane.setLayout(this.contentCardLayout);
 
-		contentPane.add(createStartPane(), "startPane");
-		
-		contentPane.add(createLoginPane(), "loginPane");
+		this.contentPane.add(this.createStartPane(), "startPane");
 
-		contentPane.add(createGamePane(), "gamePane");
+		this.contentPane.add(this.createGamePane(), "gamePane");
 		
-		contentCardLayout.show(contentPane, "startPane");
+		this.contentCardLayout.show(this.contentPane, "startPane");
 		
 
 		this.setVisible(true);
@@ -72,13 +70,13 @@ public class UIController extends JFrame implements ActionListener {
 
 	private JPanel createStartPane() {
 
-		startPane = new JPanel();
+		this.startPane = new JPanel();
 		GridBagLayout gbl_startPane = new GridBagLayout();
 		gbl_startPane.columnWidths = new int[]{0, 0, 0};
 		gbl_startPane.rowHeights = new int[]{0, 0, 0, 0};
 		gbl_startPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		gbl_startPane.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
-		startPane.setLayout(gbl_startPane);
+		this.startPane.setLayout(gbl_startPane);
 		
 		JLabel labelTitle = new JLabel("Multiplayer Snake");
 		labelTitle.setFont(new Font("Lucida Grande", Font.PLAIN, 36));
@@ -87,7 +85,7 @@ public class UIController extends JFrame implements ActionListener {
 		gbc_labelTitle.insets = new Insets(0, 0, 5, 0);
 		gbc_labelTitle.gridx = 0;
 		gbc_labelTitle.gridy = 0;
-		startPane.add(labelTitle, gbc_labelTitle);
+		this.startPane.add(labelTitle, gbc_labelTitle);
 		
 		JLabel labelNumberOfPlayers = new JLabel("Select the number of players:");
 		GridBagConstraints gbc_labelNumberOfPlayers = new GridBagConstraints();
@@ -95,60 +93,62 @@ public class UIController extends JFrame implements ActionListener {
 		gbc_labelNumberOfPlayers.insets = new Insets(0, 0, 5, 5);
 		gbc_labelNumberOfPlayers.gridx = 0;
 		gbc_labelNumberOfPlayers.gridy = 1;
-		startPane.add(labelNumberOfPlayers, gbc_labelNumberOfPlayers);
+		this.startPane.add(labelNumberOfPlayers, gbc_labelNumberOfPlayers);
 		
-		comboBoxNumberOfPlayers = new JComboBox<Integer>();
-		comboBoxNumberOfPlayers.setModel(new DefaultComboBoxModel<Integer>(new Integer[] {1, 2, 3, 4}));
-		comboBoxNumberOfPlayers.setSelectedIndex(0);
+		this.comboBoxNumberOfPlayers = new JComboBox<Integer>();
+		this.comboBoxNumberOfPlayers.setModel(new DefaultComboBoxModel<Integer>(new Integer[] {1, 2, 3, 4}));
+		this.comboBoxNumberOfPlayers.setSelectedIndex(0);
 		GridBagConstraints gbc_comboBoxNumberOfPlayers = new GridBagConstraints();
 		gbc_comboBoxNumberOfPlayers.insets = new Insets(0, 0, 5, 0);
 		gbc_comboBoxNumberOfPlayers.anchor = GridBagConstraints.WEST;
 		gbc_comboBoxNumberOfPlayers.gridx = 1;
 		gbc_comboBoxNumberOfPlayers.gridy = 1;
-		startPane.add(comboBoxNumberOfPlayers, gbc_comboBoxNumberOfPlayers);
+		this.startPane.add(this.comboBoxNumberOfPlayers, gbc_comboBoxNumberOfPlayers);
 		
-		btnStart = new JButton("Start");
+		this.btnStart = new JButton("Start");
 		GridBagConstraints gbc_btnStart = new GridBagConstraints();
 		gbc_btnStart.gridwidth = 2;
 		gbc_btnStart.insets = new Insets(0, 0, 0, 5);
 		gbc_btnStart.gridx = 0;
 		gbc_btnStart.gridy = 2;
-		startPane.add(btnStart, gbc_btnStart);
+		this.btnStart.addActionListener(this);
+		this.btnStart.setActionCommand("Start");
+		this.startPane.add(this.btnStart, gbc_btnStart);
 		return startPane;
 
 	}
 
 	private JPanel createLoginPane() {
 		
-		loginPane = new JPanel();
-		
-		return loginPane;
+		this.loginPane = new JPanel();
+		System.out.println(this.comboBoxNumberOfPlayers.getSelectedItem());
+		return this.loginPane;
 		
 	}
 	
 	private JPanel createGamePane() {
 
-		gamePane = new JPanel();
-		gamePane.setLayout(new BorderLayout(0, 0));
+		this.gamePane = new JPanel();
+		this.gamePane.setLayout(new BorderLayout(0, 0));
 
-		gamePane.add(createScorePane(), BorderLayout.WEST);
+		this.gamePane.add(createScorePane(), BorderLayout.WEST);
 
-		gamePane.add(createBoardPane(), BorderLayout.CENTER);
+		this.gamePane.add(createBoardPane(), BorderLayout.CENTER);
 
-		return gamePane;
+		return this.gamePane;
 	}
 
 	private JPanel createScorePane() {
 		
-		scorePane = new JPanel();
-		scorePane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		this.scorePane = new JPanel();
+		this.scorePane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 
 		GridBagLayout gbl_scorePane = new GridBagLayout();
 		gbl_scorePane.columnWidths = new int[]{0, 0, 0};
 		gbl_scorePane.rowHeights = new int[]{0, 0, 0};
 		gbl_scorePane.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		gbl_scorePane.rowWeights = new double[]{0.0, 0.0, 0.0};
-		scorePane.setLayout(gbl_scorePane);
+		this.scorePane.setLayout(gbl_scorePane);
 
 
 		JLabel labelScores = new JLabel("Scores");
@@ -159,57 +159,61 @@ public class UIController extends JFrame implements ActionListener {
 		gbc_labelScores.gridwidth = 2;
 		gbc_labelScores.gridx = 0;
 		gbc_labelScores.gridy = 0;
-		scorePane.add(labelScores, gbc_labelScores);
+		this.scorePane.add(labelScores, gbc_labelScores);
 
 
-		labelPlayer1Score = new JLabel("Player 1: 0");
+		this.labelPlayer1Score = new JLabel("Player 1: 0");
 		GridBagConstraints gbc_labelPlayer1Score = new GridBagConstraints();
 		gbc_labelPlayer1Score.insets = new Insets(0, 10, 0, 10);
 		gbc_labelPlayer1Score.gridx = 0;
 		gbc_labelPlayer1Score.gridy = 1;
-		scorePane.add(labelPlayer1Score, gbc_labelPlayer1Score);
+		this.scorePane.add(this.labelPlayer1Score, gbc_labelPlayer1Score);
 		
 
-		labelPlayer2Score = new JLabel("Player 2: 0");
+		this.labelPlayer2Score = new JLabel("Player 2: 0");
 		GridBagConstraints gbc_labelPlayer2Score = new GridBagConstraints();
 		gbc_labelPlayer2Score.insets = new Insets(10, 10, 0, 10);
 		gbc_labelPlayer2Score.gridx = 0;
 		gbc_labelPlayer2Score.gridy = 2;
-		scorePane.add(labelPlayer2Score, gbc_labelPlayer2Score);
+		this.scorePane.add(this.labelPlayer2Score, gbc_labelPlayer2Score);
 
 
-		labelPlayer3Score = new JLabel("Player 3: 0");
+		this.labelPlayer3Score = new JLabel("Player 3: 0");
 		GridBagConstraints gbc_labelPlayer3Score = new GridBagConstraints();
 		gbc_labelPlayer3Score.insets = new Insets(10, 10, 0, 10);
 		gbc_labelPlayer3Score.gridx = 0;
 		gbc_labelPlayer3Score.gridy = 3;
-		scorePane.add(labelPlayer3Score, gbc_labelPlayer3Score);
+		this.scorePane.add(this.labelPlayer3Score, gbc_labelPlayer3Score);
 
 
-		labelPlayer4Score = new JLabel("Player 4: 0");
+		this.labelPlayer4Score = new JLabel("Player 4: 0");
 		GridBagConstraints gbc_labelPlayer4Score = new GridBagConstraints();
 		gbc_labelPlayer4Score.anchor = GridBagConstraints.NORTH;
 		gbc_labelPlayer4Score.insets = new Insets(10, 10, 0, 10);
 		gbc_labelPlayer4Score.weighty = 1.0;
 		gbc_labelPlayer4Score.gridx = 0;
 		gbc_labelPlayer4Score.gridy = 4;
-		scorePane.add(labelPlayer4Score, gbc_labelPlayer4Score);
+		this.scorePane.add(this.labelPlayer4Score, gbc_labelPlayer4Score);
 
 
-		return scorePane;
+		return this.scorePane;
 	}
 
 	private JPanel createBoardPane() {
-		boardPane = new JPanel();
-		boardPane.setBorder(new LineBorder(new Color(0, 0, 0)));
-		boardPane.setLayout(new BorderLayout(0, 0));
-		return boardPane;
+		this.boardPane = new JPanel();
+		this.boardPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+		this.boardPane.setLayout(new BorderLayout(0, 0));
+		return this.boardPane;
 	}
 
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if (e.getActionCommand().equals("Start")) {
+			this.contentPane.add(this.createLoginPane(), "loginPane");
+			this.contentCardLayout.show(this.contentPane, "loginPane");
+			this.createLoginPane();
+		}
 		
 	}
 	
